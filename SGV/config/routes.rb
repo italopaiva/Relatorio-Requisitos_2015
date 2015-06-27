98f9ec74d9ca9c34d6752b0487daafaf5001f7aa
Matheus S. Pereira <matheussilva.fga@gmail.com>
2015-06-26 23:14:53 -0300

    Addind create vehicles

--- SGV/config/routes.rb
+++ SGV/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  
-  root :to => "login#index"
-  resources :users
 
-  post 'auth_user_path' => "login#auth_user"
+  root :to => 'application#index'
+
+  resources :vehicles
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
